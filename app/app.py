749fb43c7784609837f38edea4f0f4ad7d54225c
fstock <fs083223@gmail.com>
2018-04-14 20:04:42 +0200

    use parsed dates as index and sort

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-vehicle_states = pd.read_csv("../resources/vehicle_states.csv", sep=';')
+vehicle_states = pd.read_csv("../resources/vehicle_states.csv", sep=';', parse_dates=['last_seen','created_at'], index_col='last_seen')
+vehicle_states.sort_index(inplace=True)
 print(vehicle_states.describe())
