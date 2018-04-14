eeb463c54ee13c8ce747b49e63e226427490621c
Nice Fontaine <ulla.umme@gmail.com>
2018-04-14 16:57:29 +0200

    download and read nodes + edges from overpass

--- app/graph_downloader/graph_downloader.py
+++ app/graph_downloader/graph_downloader.py
@@ -1,50 +1,71 @@
 import requests
-import logging
 import pandas as pd
 from app.geo_tools import meters
 
-logging.basicConfig(level=logging.DEBUG)
 
-NORTH = 52.484421
-SOUTH = 52.479951
-WEST = 13.352029
-EAST = 13.359325
+DEFAULT_URL = 'https://www.overpass-api.de/api/interpreter'
+QUERY_TEMPLATE = '[out:json];way[highway][highway=service]({:f},{:f},{:f},{:f});out geom;'
 
-bbox = [SOUTH, WEST, NORTH, EAST]
 
-url = 'https://www.overpass-api.de/api/interpreter'
-params = dict(data='[out:json];way[highway][highway=service]({:f},{:f},{:f},{:f});out geom;'.format(*bbox))
+def fetch_geometries(bounding_box, url=DEFAULT_URL):
+    params = dict(data=QUERY_TEMPLATE.format(*bounding_box))
+    resp = requests.get(url=url, params=params)
+    return resp.json()
 
-resp = requests.get(url=url, params=params)
-data = resp.json()
 
-print(data)
+class Downloader:
 
-print('building nodes...')
-nodes = []
-for element in data['elements']:
-    tags = element['tags']
-    for nid, geom in zip(element['nodes'], element['geometry']):
-        nodes.append({'id': nid, 'lat': geom['lat'], 'lon': geom['lon']}) #, 'tags': tags})
+    def __init__(self, json):
+        print("downloading overpass data...")
+        self.json = json
+        self.nodes = self.__nodes()
+        self.edges = self.__edges()
 
-nodeFrame = pd.DataFrame(nodes)
-print(nodeFrame.shape)
-nodeFrame.drop_duplicates('id', inplace=True)
-nodeFrame.set_index('id', inplace=True)
-print(nodeFrame.shape)
-#print(nodeFrame)
+    def __nodes(self):
+        print('building nodes...')
+        nodes = []
+        for element in self.json['elements']:
+            for nid, geom in zip(element['nodes'], element['geometry']):
+                nodes.append({'id': nid, 'lat': geom['lat'], 'lon': geom['lon']})
+        return self.__build_nodes(nodes)
 
-print('building edges')
+    def __edges(self):
+        print('building edges...')
+        edges = []
+        for element in self.json['elements']:
+            for first, second in zip(element['nodes'][:-1], element['nodes'][1:]):
+                edges.append({'node1': first, 'node2': second})
+        return self.__build_edges(edges)
 
-edges = []
-for element in data['elements']:
-    tags = element['tags']
-    for first, second in zip(element['nodes'][:-1], element['nodes'][1:]):
-        edges.append({'node1': first, 'node2': second})
+    def __build_edges(self, edge_list):
+        edges = pd.DataFrame(edge_list)
+        edges = edges.join(self.nodes, on='node1', rsuffix='_from')
+        edges = edges.join(self.nodes, on='node2', rsuffix='_to')
+        edges['distance'] = edges.apply(lambda row: self.__distance(row), axis=1)
+        return edges[['node1', 'node2', 'distance']]
 
-edgeFrame = pd.DataFrame(edges)
-edgeFrame = edgeFrame.join(nodeFrame, on='node1', rsuffix='_from')
-edgeFrame = edgeFrame.join(nodeFrame, on='node2', rsuffix='_to')
-edgeFrame['distance'] = edgeFrame.apply(lambda row: meters(row['lat'], row['lon'], row['lat_to'], row['lon_to']), axis=1)
+    @staticmethod
+    def __build_nodes(node_list):
+        nodes = pd.DataFrame(node_list)
+        nodes.drop_duplicates('id', inplace=True)
+        nodes.set_index('id', inplace=True)
+        return nodes
 
-print(edgeFrame[['node1', 'node2']])
+    @staticmethod
+    def __distance(row):
+        return meters(row['lat'], row['lon'], row['lat_to'], row['lon_to'])
+
+
+if __name__ == '__main__':
+    NORTH = 52.484421
+    SOUTH = 52.479951
+    WEST = 13.352029
+    EAST = 13.359325
+
+    json = fetch_geometries([SOUTH, WEST, NORTH, EAST])
+
+    downloader = Downloader(json)
+    print("*** NODES ***")
+    print(downloader.nodes)
+    print("*** EDGES ***")
+    print(downloader.edges)
