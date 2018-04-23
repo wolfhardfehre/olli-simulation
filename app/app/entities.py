046eaca9d4e0b7822ac6d7ec160baa17f32ac218
Nice Fontaine <wolfhard.fehre@motion-tag.com>
2018-04-23 08:25:37 +0200

    Use shapely (#15)
    * set up shapely for graph
    * add random walk
    * delete pkg-resources
    * updated psycopg2 to 2.7 due to a bug

--- app/app/entities.py
+++ app/app/entities.py
@@ -1,13 +1,11 @@
 import math
 import abc
 import random
-
 import pandas as pd
-
-
+from shapely.geometry import Point, LineString
+from graph import Graph
 # TODO velocity model (curve, surface, mean/std)
 # TODO battery model (battery consumption)
-from graph import Graph
 
 LATITUDE_APPROX = 111320.0
 
@@ -28,24 +26,30 @@ class Shuttle(Entity):
 
     def __init__(self, graph, time, velocity_model):
         super().__init__(graph, time)
-        node, edge = graph.seed()
-        start = graph.nodes.loc[edge.iloc[0]['node1']]
-        end = graph.nodes.loc[edge.iloc[0]['node2']]
-        self.a = math.atan2(end['lat'] - start['lat'], end['lon'] - start['lon'])
-        self.x = node['lon'].values[0]
-        self.y = node['lat'].values[0]
+        self.position, self.edge, self.a, self.end_id = self.__set_edge(graph.seed())
         self.velocity_model = velocity_model
 
     def current_position(self):
-        return self.x, self.y
+        return self.position
 
     def move(self, current_time):
-        delta_time = current_time - self.time
-        degrees_per_second = self.velocity_model.current_velocity()
-        vx = degrees_per_second * math.cos(self.y)
-        vy = degrees_per_second
-        self.x = self.x + delta_time * vx * math.cos(self.a)
-        self.y = self.y + delta_time * vy * math.sin(self.a)
+        delta_degrees = self.velocity_model.current_velocity() * (current_time - self.time)
+        x = self.position.x + delta_degrees * math.cos(self.position.y) * math.cos(self.a)
+        y = self.position.y + delta_degrees * math.sin(self.a)
+        self.position = Point((x, y))
+        if not self.position.within(self.edge):
+            self.__pick_next()
+
+    def __pick_next(self):
+        edge = self.graph.get_adjacent_to(self.end_id)
+        self.position, self.edge, self.a, self.end_id = self.__set_edge(edge)
+
+    @staticmethod
+    def __set_edge(edge):
+        start, end, end_id = edge
+        azimuth = math.atan2(end.y - start.y, end.x - start.x)
+        edge = LineString([start, end])
+        return start, edge, azimuth, end_id
 
 
 class VelocityModel:
@@ -62,12 +66,9 @@ if __name__ == '__main__':
     node_data = [['1', 52.3, 13.4], ['2', 52.4, 13.4], ['3', 52.4, 13.3], ['4', 52.3, 13.3]]
     edge_data = [['1', '2', 30], ['2', '3', 20], ['3', '4', 45], ['4', '1', 25]]
     nodes = pd.DataFrame(node_data, columns=['id', 'lat', 'lon'])
-    nodes.set_index('id')
     edges = pd.DataFrame(edge_data, columns=['node1', 'node2', 'distance'])
-    sample_graph = Graph(nodes, edges)
-    velocity = VelocityModel()
-    shuttle = Shuttle(sample_graph, 0, velocity)
-    print(*shuttle.current_position())
-    for t in range(0, 20000, 1000):
+    shuttle = Shuttle(Graph(nodes, edges), 0, VelocityModel())
+    print(shuttle.current_position())
+    for t in range(0, 100):
         shuttle.move(20)
-        print(*shuttle.current_position())
+        print(shuttle.current_position())
--- app/app/graph.py
+++ app/app/graph.py
@@ -1,26 +1,52 @@
 import pandas as pd
+import random
+from geopandas import GeoDataFrame
+from shapely.geometry import Point
 
 
 class Graph:
 
     def __init__(self, nodes, edges):
-        self.nodes = nodes
-        self.nodes.set_index('id', inplace=True)
-        copy = edges.copy(deep=True)
-        copy.columns = ['node2', 'node1', 'distance']
-        self.edges = pd.concat([edges, copy]).reset_index()[['node1', 'node2', 'distance']]
-        self.adjacent = self.edges.node1.groupby(self.edges.node2).apply(list)
+        self.nodes = self.__create_nodes(nodes)
+        self.adjacent = self.__create_adjacent(edges)
+
+    def get_adjacent_to(self, start_id):
+        end_id = self.__seed_end_node(start_id)
+        end = self.node_by_id(end_id)
+        start = self.node_by_id(start_id)
+        return start.geometry, end.geometry, end_id
 
     def seed(self):
-        node = self.__seed_node()
-        edge = self.__seed_edge(node)
-        return node, edge
+        start = self.__seed_start_node()
+        end_id = self.__seed_end_node(start.index.values[0])
+        end = self.node_by_id(end_id)
+        return start.iloc[0].geometry, end.geometry, end_id
 
-    def __seed_node(self):
+    def node_by_id(self, id):
+        return self.nodes.loc[id]
+
+    def __seed_start_node(self):
         return self.nodes.sample(n=1)
 
-    def __seed_edge(self, node):
-        return self.edges[self.edges.node1 == node.index.values[0]].sample(n=1)
+    def __seed_end_node(self, node):
+        return random.choice(self.adjacent[node])
+
+    @staticmethod
+    def __create_nodes(nodes):
+        geometry = [Point(xy) for xy in zip(nodes.lat, nodes.lon)]
+        nodes = GeoDataFrame(nodes.drop(['lat', 'lon'], axis=1),
+                             crs={'init': 'epsg:4326'},
+                             geometry=geometry)
+        nodes.set_index('id', inplace=True)
+        return nodes
+
+    @staticmethod
+    def __create_adjacent(edges):
+        copy = edges.copy(deep=True)
+        copy.columns = ['node2', 'node1', 'distance']
+        edges = pd.concat([edges, copy]).reset_index()[['node1', 'node2', 'distance']]
+        adjacent = edges.node1.groupby(edges.node2).apply(list)
+        return {i: row for i, row in adjacent.iteritems()}
 
 
 if __name__ == '__main__':
