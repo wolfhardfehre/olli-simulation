fea0212604f8d84df51f95b81c8839fc0116f35e
Nice Fontaine <ulla.umme@gmail.com>
2018-04-23 08:15:42 +0200

    fixed test

--- tests/app/test_graph.py
+++ tests/app/test_graph.py
@@ -7,7 +7,6 @@ from app.app.graph import Graph
 class GraphTest(unittest.TestCase):
 
     def test_adjacent_list(self):
-        expected = [['4', '2'], ['1', '3'], ['2', '4'], ['3', '1']]
         node_data = [['1', 52.3, 13.4], ['2', 52.4, 13.4], ['3', 52.4, 13.3], ['4', 52.3, 13.3]]
         edge_data = [['1', '2', 30], ['2', '3', 20], ['3', '4', 45], ['4', '1', 25]]
         nodes = pd.DataFrame(node_data, columns=['id', 'lat', 'lon'])
@@ -16,8 +15,11 @@ class GraphTest(unittest.TestCase):
         graph = Graph(nodes, edges)
 
         self.assertEqual(4, len(graph.nodes.index))
-        self.assertEqual(8, len(graph.edges.index))
-        [self.assertEqual(results, elements) for results, elements in zip(expected, graph.adjacent)]
+        self.assertEqual(4, len(graph.adjacent))
+        self.assertEqual(['4', '2'], graph.adjacent['1'])
+        self.assertEqual(['1', '3'], graph.adjacent['2'])
+        self.assertEqual(['2', '4'], graph.adjacent['3'])
+        self.assertEqual(['3', '1'], graph.adjacent['4'])
 
 
 if __name__ == '__main__':
