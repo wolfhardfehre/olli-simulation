424858ed9d0684a14aa9a866cb5fcc09fc33fdd5
Nice Fontaine <ulla.umme@gmail.com>
2018-05-01 11:44:08 +0200

    fetch and load charge stations

--- app/downloaders/charge_station_downloader.py
+++ app/downloaders/charge_station_downloader.py
@@ -1,40 +1,62 @@
 import requests
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from geoalchemy2 import WKTElement
+import pandas as pd
 
 
 BASE_URL = 'http://data.datarun2018.de/EVCharging/stations'
 
 
-engine = create_engine('postgresql:///open_olli', echo=False)
-Base = declarative_base(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-
-
 def fetch_stations(lat, lon, radius, detail='low'):
-    params = {
-        'lat': lat,
-        'lng': lon,
-        'radius': radius,
-        'detail': detail
-    }
+    """
+    Fetch charging stations around a certain coordinate within a specified radius.
+    e.g. http://data.datarun2018.de/EVCharging/stations?lat=52.4590335&lng=13.3738759&radius=20000&detail=low
+
+    Parameters
+    ----------
+    lat: float
+        latitude in degrees
+    lon: float
+        longitude in degrees
+    radius: int
+        radius in meters
+    detail: str
+        either 'low' or 'high' for level of detail
+
+    Returns
+    -------
+    json: containing the charge stations
+
+    """
+    params = {'lat': lat, 'lng': lon, 'radius': radius, 'detail': detail}
     response = requests.get(url=BASE_URL, params=params)
     return response.json()
 
 
-class ChargeStation(Base):
-    """Mapping for charge stations data (existing table)"""
-    __tablename__ = 'charge_stations'
-    __table_args__ = {'autoload': True}
+class ChargeStationDownloader:
+
+    def __init__(self, json):
+        self.charge_stations = self.__build_stations(json)
+        print(self.charge_stations)
+
+    def __build_stations(self, json):
+        df = pd.DataFrame(columns=['id','name', 'data_type', 'lat', 'lon'])
+        for station in json:
+            df = df.append(self.__to_row(station), ignore_index=True)
+        return df
+
+    @staticmethod
+    def __to_row(station):
+        lon, lat = station['location']['coordinates']
+        return {'id': station['id'],
+                'name': station['name'],
+                'data_type': station['dataType'],
+                'lat': lat,
+                'lon': lon}
+
+    def save(self):
+        self.charge_stations.to_pickle('./resources/charge_stations.p')
 
 
 if __name__ == '__main__':
-    json = fetch_stations(52.481528, 13.356441, 20000)
-    for station in json:
-        lng, lat = station['location']['coordinates']
-        wkt = WKTElement('POINT({} {})'.format(lng, lat), 4326)
-        session.add(ChargeStation(data_type=station['dataType'], name=station['name'], geometry=wkt))
-    session.commit()
+    js = fetch_stations(52.481528, 13.356441, 20000)
+    downloader = ChargeStationDownloader(js)
+    downloader.save()
